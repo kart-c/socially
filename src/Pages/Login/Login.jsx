60c65037dd50432d9ce8d7bff0b31973b232709c
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-13 22:31:33 +0530

    feat: add login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,43 @@
+import {
+	Button,
+	Checkbox,
+	Container,
+	Flex,
+	FormControl,
+	FormLabel,
+	Input,
+	Link,
+	Text,
+} from '@chakra-ui/react';
+import React from 'react';
+
+const Login = () => {
+	return (
+		<Container maxW={320}>
+			<Flex height="100vh" justifyContent="center" maxW="full" flexDir="column">
+				<FormControl>
+					<FormLabel my="3">Username</FormLabel>
+					<Input type="text" placeholder="Oogway" />
+				</FormControl>
+				<FormControl>
+					<FormLabel my="3">Password</FormLabel>
+					<Input type="password" placeholder="********" />
+				</FormControl>
+				<Checkbox type="checkbox" alignSelf="flex-start" my="3">
+					Remember Me
+				</Checkbox>
+				<Button w="full" mb="4" variant="brand">
+					Login
+				</Button>
+				<Text>
+					Not a user?
+					<Link href="/signup" color="brand.500" px="2">
+						Signup
+					</Link>
+				</Text>
+			</Flex>
+		</Container>
+	);
+};
+
+export { Login };
