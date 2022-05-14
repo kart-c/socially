b0083f28f54252594f2670186639130eb056807e
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-14 15:32:51 +0530

    refactor: hide aside and left nav on login/signup pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Container, Box } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { LeftNav, RightAside } from 'Components';
 import { Home, Login, Signup } from 'Pages';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
+	const { pathname } = useLocation();
 	return (
-		<Container as="main" maxW="container.xl" mx="auto" display="flex">
-			<LeftNav />
+		<Container
+			as="main"
+			maxW="container.xl"
+			mx="auto"
+			display="flex"
+			justifyContent={
+				pathname.slice(1) === 'login' || pathname.slice(1) === 'signup' ? 'center' : 'unset'
+			}
+		>
+			{pathname.slice(1) === 'login' || pathname.slice(1) === 'signup' ? null : <LeftNav />}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
 			</Routes>
-			<RightAside />
+			{pathname.slice(1) === 'login' || pathname.slice(1) === 'signup' ? null : <RightAside />}
 		</Container>
 	);
 }
