10d52d8b5a4826b22de2ac5d9fe02258bf674504
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-15 22:52:51 +0530

    feat: add redux store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { makeServer } from './server';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from 'theme/theme';
+import { store } from 'Redux/app/store';
 
 // Call make Server
 makeServer();
@@ -13,10 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<ChakraProvider theme={theme}>
-				<App />
-			</ChakraProvider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<ChakraProvider theme={theme}>
+					<App />
+				</ChakraProvider>
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 );
