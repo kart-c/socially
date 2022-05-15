3aff066d0eab820a9424dc6b83af9d7e04efe4d0
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-15 23:29:31 +0530

    feat: add signup slice

--- src/Redux/Slice/authSlice.js
+++ src/Redux/Slice/authSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { login } from 'Redux/Thunk';
+import { signup } from 'Redux/Thunk';
 
 const initialState = {
 	user: null,
@@ -22,6 +23,19 @@ const authSlice = createSlice({
 		},
 		[login.rejected]: (state, action) => {
 			state.isLoading = false;
+			console.error(action);
+		},
+		[signup.pending]: (state, action) => {
+			state.isLoading = true;
+		},
+		[signup.fulfilled]: (state, { payload }) => {
+			state.isLoading = false;
+			state.token = payload.data.encodedToken;
+			state.user = payload.data.createdUser;
+		},
+		[signup.rejected]: (state, action) => {
+			state.isLoading = false;
+			console.error(action);
 		},
 	},
 });
