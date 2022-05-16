72c1fd74e1419d31575e36c4d6f745b751c183ac
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-16 15:48:16 +0530

    feat: show posts on home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 import { FiPlusCircle } from 'react-icons/fi';
-import { PgWrapper, Post } from 'Components';
+import { Loader, PgWrapper, Post } from 'Components';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts } from 'Redux/Thunk';
 
 const Home = ({ onOpen }) => {
+	const dispatch = useDispatch();
+	const { isLoading } = useSelector((state) => state.users);
+	const { posts, isLoading: postLoading } = useSelector((state) => state.posts);
+
+	const getPosts = async () => await dispatch(getAllPosts());
+
+	useEffect(() => {
+		getPosts();
+	}, []);
 	return (
 		<>
 			<PgWrapper>
-				<Box px="4" pb="4" borderBottom="1px solid" borderColor="gray.200">
-					<Text as="h2" fontWeight="700" mb="2.5">
-						Home
-					</Text>
-					<Flex gap="3" align="flex-end">
-						<Avatar name="Kartik Choudhary" src="https://" />
-						<Text fontSize="lg" mb="1" color="gray.300" cursor="text" flexGrow="1" onClick={onOpen}>
-							What's Happening?
+				{isLoading || postLoading ? (
+					<Loader />
+				) : (
+					<Box px="4" pb="4" borderBottom="1px solid" borderColor="gray.200">
+						<Text as="h2" fontWeight="700" mb="2.5">
+							Home
 						</Text>
-						<Box color="brand.500" bgColor="#fff" mb="1.5" cursor="pointer" onClick={onOpen}>
-							<FiPlusCircle fontSize="24px" />
-						</Box>
-					</Flex>
-				</Box>
-				<Post />
-				<Post img={'https://www.picsum.photos/300'} />
-				<Post />
-				<Post />
+						<Flex gap="3" align="flex-end">
+							<Avatar name="Kartik Choudhary" src="https://" />
+							<Text
+								fontSize="lg"
+								mb="1"
+								color="gray.300"
+								cursor="text"
+								flexGrow="1"
+								onClick={onOpen}
+							>
+								What's Happening?
+							</Text>
+							<Box color="brand.500" bgColor="#fff" mb="1.5" cursor="pointer" onClick={onOpen}>
+								<FiPlusCircle fontSize="24px" />
+							</Box>
+						</Flex>
+					</Box>
+				)}
+				{posts?.length > 0 ? (
+					posts.map((post) => <Post key={post._id} {...post} />)
+				) : (
+					<div>No posts</div>
+				)}
 			</PgWrapper>
 		</>
 	);
