9baa88726b216624eb2a2ba5b0e5ed62b9d87f4d
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-16 22:54:02 +0530

    feat: add profile page data

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
 	Avatar,
 	Button,
@@ -13,62 +13,96 @@ import {
 } from '@chakra-ui/react';
 import { MdLogout } from 'react-icons/md';
 import { PgWrapper, Post, ProfileModal } from 'Components';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+	const { user } = useSelector((state) => state.auth);
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [userObj, setUserObj] = useState();
+	const { username } = useParams();
+	const [userPosts, setUserPosts] = useState([]);
+
+	const getUser = async () => {
+		const response = await axios.get(`/api/users/${user._id}`);
+		setUserObj(response.data.user);
+	};
+
+	const getUserPosts = async () => {
+		const response = await axios.get(`/api/posts/user/${username}`);
+		setUserPosts(response.data.posts);
+	};
+
+	console.log(userPosts);
+
+	useEffect(() => {
+		getUser();
+	}, []);
+
+	useEffect(() => {
+		getUserPosts();
+	}, []);
 
 	return (
 		<PgWrapper>
-			<ProfileModal isOpen={isOpen} onClose={onClose} />
-			<Flex
-				p="4"
-				align="flex-start"
-				gap="3"
-				wrap={{ base: 'wrap', sm: 'nowrap' }}
-				borderBottom="1px solid"
-				borderColor="gray.200"
-			>
-				<Avatar name="Kartik Choudhary" src="https://" size="lg" />
-				<VStack align="flex-start">
-					<Heading as="h2" fontSize="24px">
-						Kartik Choudhary
-					</Heading>
-					<Text as="span" color="gray.300" fontSize="14px">
-						@kart_c11
-					</Text>
-					<Text>A novice web developer</Text>
-					<Flex gap="4">
-						<Text as="span">0 Posts</Text>
-						<Text as="span">0 Followers</Text>
-						<Text as="span">0 Following</Text>
-					</Flex>
-					<Link href="https://github.com/kart-c" isExternal color="brand.500" fontSize="14px">
-						https://github.com/kart-c
-					</Link>
-				</VStack>
-				<Flex
-					flexDir={{ base: 'row', sm: 'column' }}
-					align="flex-end"
-					gap="4"
-					ml="auto"
-					w={{ base: '100%', sm: 'auto' }}
-				>
-					<Button variant="outline" border="2px solid" borderColor="brand.500" onClick={onOpen}>
-						Edit
-					</Button>
-					<Tooltip label="Logout">
-						<IconButton
-							icon={<MdLogout />}
-							aria-label="Logout"
-							border="2px solid"
-							borderColor="brand.500"
-							variant="outline"
+			{userObj?.username ? (
+				<>
+					<ProfileModal isOpen={isOpen} onClose={onClose} />
+					<Flex
+						p="4"
+						align="flex-start"
+						gap="3"
+						wrap={{ base: 'wrap', sm: 'nowrap' }}
+						borderBottom="1px solid"
+						borderColor="gray.200"
+					>
+						<Avatar
+							name={`${userObj.firstName} ${userObj.lastName}`}
+							src={user.profilePic}
+							size="lg"
 						/>
-					</Tooltip>
-				</Flex>
-			</Flex>
-			{/* <Post />
-			<Post /> */}
+						<VStack align="flex-start">
+							<Heading as="h2" fontSize="24px">
+								{`${userObj.firstName} ${userObj.lastName}`}
+							</Heading>
+							<Text as="span" color="gray.300" fontSize="14px">
+								@{user.username}
+							</Text>
+							<Text>{user.bio}</Text>
+							<Flex gap="4">
+								<Text as="span">0 Posts</Text>
+								<Text as="span">{user.followers.length} Followers</Text>
+								<Text as="span">{user.following.length} Following</Text>
+							</Flex>
+							<Link href={user.link} isExternal color="brand.500" fontSize="14px">
+								{user.link}
+							</Link>
+						</VStack>
+						<Flex
+							flexDir={{ base: 'row', sm: 'column' }}
+							align="flex-end"
+							gap="4"
+							ml="auto"
+							w={{ base: '100%', sm: 'auto' }}
+						>
+							<Button variant="outline" border="2px solid" borderColor="brand.500" onClick={onOpen}>
+								Edit
+							</Button>
+							<Tooltip label="Logout">
+								<IconButton
+									icon={<MdLogout />}
+									aria-label="Logout"
+									border="2px solid"
+									borderColor="brand.500"
+									variant="outline"
+								/>
+							</Tooltip>
+						</Flex>
+					</Flex>
+					{userPosts.length > 0 ? userPosts.map((post) => <Post {...post} key={post._id} />) : null}
+				</>
+			) : null}
 		</PgWrapper>
 	);
 };
