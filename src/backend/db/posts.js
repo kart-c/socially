6d2b3bcbfbd2cd08681db1d721ff7b5036687422
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-16 17:36:53 +0530

    chore: add more posts

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -12,7 +12,22 @@ export const posts = [
 		content: 'What have I got in my pocket?',
 		likes: {
 			likeCount: 0,
-			likedBy: [],
+			likedBy: [
+				{
+					firstName: 'Bilbo',
+					lastName: 'Baggins',
+					username: 'hobbit',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				},
+				{
+					firstName: 'Bruce',
+					lastName: 'Wayne',
+					username: 'not_batman',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652699201/Socially/Profiles/Bruce.jpg',
+				},
+			],
 			dislikedBy: [],
 		},
 		firstName: 'Bilbo',
@@ -21,8 +36,21 @@ export const posts = [
 		createdAt: formatDate(),
 		updatedAt: formatDate(),
 		profilePic:
-			'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
 		comments: [
+			{
+				_id: uuid(),
+				firstName: 'Smeagol',
+				lastName: 'Smeagol',
+				username: 'gollum',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652698709/Socially/Profiles/gollum.jpg',
+				text: "That's no fair, that's no fair, that's against the rules.",
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
 			{
 				_id: uuid(),
 				firstName: 'Adarsh',
@@ -36,19 +64,6 @@ export const posts = [
 					downvotedBy: [],
 				},
 			},
-			{
-				_id: uuid(),
-				firstName: 'Smeagol',
-				lastName: 'Smeagol',
-				username: 'gollum',
-				profilePic:
-					'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
-				text: "That's no fair, that's no fair, that's against the rules.",
-				votes: {
-					upvotedBy: [],
-					downvotedBy: [],
-				},
-			},
 		],
 	},
 	{
@@ -57,7 +72,22 @@ export const posts = [
 			'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.',
 		likes: {
 			likeCount: 0,
-			likedBy: [],
+			likedBy: [
+				{
+					firstName: 'Bilbo',
+					lastName: 'Baggins',
+					username: 'hobbit',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				},
+				{
+					firstName: 'Gandalf',
+					lastName: 'Grey',
+					username: 'wizard',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698930/Socially/Profiles/gandalf.jpg',
+				},
+			],
 			dislikedBy: [],
 		},
 		firstName: 'Kartik',
@@ -72,7 +102,7 @@ export const posts = [
 				lastName: 'Baggins',
 				username: 'hobbit',
 				profilePic:
-					'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
 				text: 'Interesting',
 				votes: {
 					upvotedBy: [],
@@ -85,7 +115,7 @@ export const posts = [
 				lastName: 'Grey',
 				username: 'wizard',
 				profilePic:
-					'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652698930/Socially/Profiles/gandalf.jpg',
 				text: 'Wow!',
 				votes: {
 					upvotedBy: [],
@@ -95,5 +125,227 @@ export const posts = [
 		],
 		createdAt: formatDate(),
 		updatedAt: formatDate(),
+	},
+	{
+		_id: uuid(),
+		content: "It's over, Anakin! I have the high ground.",
+		likes: {
+			likeCount: 0,
+			likedBy: [],
+			dislikedBy: [
+				{
+					firstName: 'Anakin',
+					lastName: 'Skywalker',
+					username: 'vader',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652699069/Socially/Profiles/anakin.jpg',
+				},
+			],
+		},
+		firstName: 'Obiwan',
+		lastName: 'Kenobi',
+		username: 'ben',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+		profilePic:
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652699372/Socially/Profiles/kenobi.png',
+		comments: [
+			{
+				_id: uuid(),
+				firstName: 'Anakin',
+				lastName: 'Skywalker',
+				username: 'vader',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652699069/Socially/Profiles/anakin.jpg',
+				text: 'You underestimate my power.',
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
+			{
+				_id: uuid(),
+				firstName: 'Bruce',
+				lastName: 'Wayne',
+				username: 'not_batman',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652699201/Socially/Profiles/Bruce.jpg',
+				text: 'This is an interesting turn of events.',
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
+		],
+	},
+	{
+		_id: uuid(),
+		content: 'You shall not pass',
+		likes: {
+			likeCount: 0,
+			likedBy: [
+				{
+					firstName: 'Kartik',
+					lastName: 'Choudhary',
+					username: 'kart_c11',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+				},
+				{
+					firstName: 'Bilbo',
+					lastName: 'Baggins',
+					username: 'hobbit',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				},
+				{
+					firstName: 'Bruce',
+					lastName: 'Wayne',
+					username: 'not_batman',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652699201/Socially/Profiles/Bruce.jpg',
+				},
+				{
+					firstName: 'Obiwan',
+					lastName: 'Kenobi',
+					username: 'ben',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652699372/Socially/Profiles/kenobi.png',
+				},
+			],
+			dislikedBy: [],
+		},
+		firstName: 'Gandalf',
+		lastName: 'Grey',
+		username: 'wizard',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+		profilePic:
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652698930/Socially/Profiles/gandalf.jpg',
+		comments: [],
+	},
+	{
+		_id: uuid(),
+		content: 'Good Morning everyone. Have a nice day',
+		likes: {
+			likeCount: 0,
+			likedBy: [
+				{
+					firstName: 'Kartik',
+					lastName: 'Choudhary',
+					username: 'kart_c11',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+				},
+				{
+					firstName: 'Bilbo',
+					lastName: 'Baggins',
+					username: 'hobbit',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				},
+			],
+			dislikedBy: [],
+		},
+		firstName: 'Adarsh',
+		lastName: 'Balika',
+		username: 'adarshbalika',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+		profilePic:
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+		comments: [
+			{
+				_id: uuid(),
+				firstName: 'Anakin',
+				lastName: 'Skywalker',
+				username: 'vader',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652699069/Socially/Profiles/anakin.jpg',
+				text: 'You too.',
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
+		],
+	},
+	{
+		_id: uuid(),
+		content: 'Only a small part is played in great deeds by any hero.',
+		likes: {
+			likeCount: 0,
+			likedBy: [
+				{
+					firstName: 'Kartik',
+					lastName: 'Choudhary',
+					username: 'kart_c11',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652687739/Socially/Profiles/avataaars_jhvh1w.png',
+				},
+				{
+					firstName: 'Bilbo',
+					lastName: 'Baggins',
+					username: 'hobbit',
+					profilePic:
+						'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				},
+			],
+			dislikedBy: [],
+		},
+		firstName: 'Gandalf',
+		lastName: 'Grey',
+		username: 'wizard',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+		profilePic:
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652698930/Socially/Profiles/gandalf.jpg',
+		comments: [
+			{
+				_id: uuid(),
+				firstName: 'Bruce',
+				lastName: 'Wayne',
+				username: 'not_batman',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652699201/Socially/Profiles/Bruce.jpg',
+				text: 'You sure about that?',
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
+		],
+	},
+	{
+		_id: uuid(),
+		content:
+			'We wants it. We needs it. Must have the precious. They stole it from us. Sneaky little Hobbitses. Wicked. Tricksy. False.',
+		likes: {
+			likeCount: 0,
+			likedBy: [],
+			dislikedBy: [],
+		},
+		firstName: 'Smeagol',
+		lastName: 'Smeogol',
+		username: 'gollum',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+		profilePic:
+			'https://res.cloudinary.com/obi-wan/image/upload/v1652698709/Socially/Profiles/gollum.jpg',
+		comments: [
+			{
+				_id: uuid(),
+				firstName: 'Bilbo',
+				lastName: 'Baggins',
+				username: 'hobbit',
+				profilePic:
+					'https://res.cloudinary.com/obi-wan/image/upload/v1652698572/Socially/Profiles/bilbo.jpg',
+				text: 'Hahaha',
+				votes: {
+					upvotedBy: [],
+					downvotedBy: [],
+				},
+			},
+		],
 	},
 ];
