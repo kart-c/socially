ccdcfc1ff3c9aa4272ab172e7f44bdfb11416adf
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-17 23:35:21 +0530

    refactor: add try catch blocks for all thunks

--- src/Redux/Thunk/usersThunk.js
+++ src/Redux/Thunk/usersThunk.js
@@ -1,10 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getUsers = createAsyncThunk('users/all', async () => {
-	const response = await axios.get('/api/users');
-	const data = { data: response.data, status: response.status };
-	return data;
+export const getUsers = createAsyncThunk('users/all', async ({ rejectWithValue }) => {
+	try {
+		const response = await axios.get('/api/users');
+		const data = { data: response.data, status: response.status };
+		return data;
+	} catch (error) {
+		return rejectWithValue({ status: error.response.status, data: error.response.data });
+	}
 });
 
 export const follow = createAsyncThunk(
