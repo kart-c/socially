d0aa920067261346caf57534c101dab114a47d39
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-19 10:56:01 +0530

    refactor: fetch bookmark posts from backend

--- src/Pages/Bookmark/Bookmark.jsx
+++ src/Pages/Bookmark/Bookmark.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import { PgWrapper, Post } from 'Components';
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 const Bookmark = ({ onOpen }) => {
-	const { user } = useSelector((state) => state.auth);
+	const [bookmarks, setBookmarks] = useState([]);
+	const { token, user } = useSelector((state) => state.auth);
+	const { posts } = useSelector((state) => state.posts);
+
+	useEffect(() => {
+		const getBookmarks = async () => {
+			const response = await axios.get('/api/users/bookmark', {
+				headers: { authorization: token },
+			});
+			setBookmarks(response.data.bookmarks);
+		};
+		getBookmarks();
+	}, [posts, token, user]);
+
+	const bookmarkedPosts = posts.filter((post) =>
+		bookmarks.some((bookmark) => bookmark._id === post._id)
+	);
 
 	return (
 		<PgWrapper>
-			{user.bookmarks.length > 0 ? (
-				user.bookmarks.map((post) => <Post key={post._id} {...post} onOpen={onOpen} />)
+			{bookmarkedPosts.length > 0 ? (
+				bookmarkedPosts.map((post) => <Post key={post._id} {...post} onOpen={onOpen} />)
 			) : (
 				<Box textAlign="center" mt="16">
 					Bookmark posts to see here
