405a50ac6f9388bd88dde68c687a8f5c12b51c98
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-06-01 17:29:38 +0530

    feat: add lazy loading in explore page

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,43 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { Loader, PgWrapper, Post, Search, SortContainer } from 'Components';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { Loader, PgWrapper, Post, Search } from 'Components';
+import { Box, Text } from '@chakra-ui/react';
+import axios from 'axios';
 import { getAllPosts } from 'Redux/Thunk';
-import { sortByTrending } from 'Utils/sortByTrending';
-import { sortByDate } from 'Utils/sortByDate';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Explore = ({ onOpen }) => {
-	const [trending, setTrending] = useState(false);
-	const [sortBy, setSortBy] = useState('');
+	const [posts, setPosts] = useState([]);
+	const [pgLoading, setPgLoading] = useState(true);
+	const [loadPosts, setLoadPosts] = useState(false);
+	const [pageEnd, setPageEnd] = useState(false);
+	const [pgNumber, setPgNumber] = useState(1);
+	const intersectionRef = useRef();
+	const { posts: allPosts, status } = useSelector((state) => state.posts);
 	const dispatch = useDispatch();
-	const { posts, status } = useSelector((state) => state.posts);
 
 	useEffect(() => {
 		if (status === 'idle') {
 			const getPosts = async () => await dispatch(getAllPosts());
 			getPosts();
 		}
-	}, [dispatch, status]);
+	});
 
-	const trendingSort = sortByTrending(posts, trending);
+	useEffect(() => {
+		const ref = intersectionRef.current;
+		const intersectionHandler = (entries) => {
+			if (entries[0].isIntersecting) {
+				setPgNumber((prev) => prev + 1);
+			}
+		};
+		const observer = new IntersectionObserver(intersectionHandler);
+		if (ref) observer.observe(ref);
 
-	const dateSort = sortByDate(trendingSort, sortBy);
+		return () => observer.unobserve(ref);
+	}, []);
+
+	useEffect(() => {
+		if (!pageEnd) {
+			const getPosts = async () => {
+				try {
+					setLoadPosts(true);
+					const response = await axios.get(`/api/explore/${pgNumber}`);
+					if (response.status === 200) {
+						if (response.data.posts.length === posts.length) setPageEnd(true);
+						setPosts(response.data.posts);
+					}
+				} catch (error) {
+					console.error(error);
+				} finally {
+					setPgLoading(false);
+					setLoadPosts(false);
+				}
+			};
+			getPosts();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [pgNumber, pageEnd]);
+
+	const filteredPosts = allPosts.filter((post) => posts.some((item) => item._id === post._id));
 
 	return (
 		<PgWrapper>
-			{status === 'idle' ? <Loader /> : null}
 			<Search />
-			{posts?.length > 0 ? (
+			{pgLoading ? <Loader /> : null}
+			{filteredPosts?.length > 0 ? (
 				<>
-					<SortContainer
-						setTrending={setTrending}
-						trending={trending}
-						sortBy={sortBy}
-						setSortBy={setSortBy}
-					/>
-					{dateSort.map((post) => (
+					{filteredPosts.map((post) => (
 						<Post key={post._id} {...post} onOpen={onOpen} />
 					))}
 				</>
+			) : null}
+			<Box w="100%" ref={intersectionRef} h="20px"></Box>
+			{loadPosts ? <Loader height="100px" /> : null}
+			{!pgLoading && pageEnd ? (
+				<Text mb="4" pb={{ base: '16', mySm: '0' }} textAlign="center">
+					Page End !
+				</Text>
 			) : null}
 		</PgWrapper>
 	);
